aa856a3bba7d196b0cf385b9753d1e4cce13b5a8
Gabi Purcaru <gabi@purcaru.com>
2012-01-02 23:34:10 +0200

    added markdown support

--- messages.js
+++ messages.js
@@ -1,12 +1,17 @@
 var mongoose = require('mongoose');
+var markdown = require('markdown');
 var Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost/tracker');
 
+var markdown_parse = function(text) {
+    return markdown.markdown.toHTML(text);
+}
+
 var MessageSchema = new Schema({
     author: { "type": String, "default": "John Doe" },
     date: { "type": Date, "default": new Date() },
-    content: { "type": String, "default": "o hai <br/> o hai </br> ooo haiaiiasdias </br></br> hai" },
+    content: { "type": String, "default": "o hai <br/> o hai </br> ooo haiaiiasdias </br></br> hai", "get": markdown_parse },
 });
 var Message = mongoose.model('Message', MessageSchema);
 
