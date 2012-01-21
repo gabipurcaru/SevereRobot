dc70c718dc418f31f2593ab8cac7e250e3aa2d03
Gabi Purcaru <gabi@purcaru.com>
2012-01-21 12:05:56 +0200

    modified settings to suit herkou

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
  * General settings
  */
 
-exports.PORT = 3000;
-exports.DB = 'mongodb://localhost/tracker2';
+exports.PORT = process.env.PORT || 3000;
+exports.DB = process.env.MONGOLAB_URI || 'mongodb://localhost/tracker2';
 exports.VIEWS = __dirname + '/views';
 exports.PUBLIC = __dirname + '/public';
