b83ad3331d9edc30cebb6d3f0d0d949078cede29
Gabi Purcaru <gabi@purcaru.com>
2012-01-28 12:31:05 +0200

    fixed settings

--- settings.js
+++ settings.js
@@ -6,4 +6,4 @@ exports.PORT = process.env.PORT || 3000;
 exports.DB = process.env.MONGOLAB_URI || 'mongodb://localhost/tracker2';
 exports.VIEWS = __dirname + '/views';
 exports.PUBLIC = __dirname + '/public';
-exports.LOGIN_FULL_URL = process.env.ROOT_URL || 'http://gabipurcaru.dyndns.org:5000/login/';
+exports.LOGIN_FULL_URL = process.env.ROOT_URL + '/login/'  || 'http://gabipurcaru.dyndns.org:5000/login/';
