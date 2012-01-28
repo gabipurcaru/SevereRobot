56b88f575565144e713c20e53f06cedba3275867
Gabi Purcaru <gabi@purcaru.com>
2012-01-28 10:58:56 +0200

    improved frontend

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,14 +7,37 @@ $(function() {
             window.location.replace(window.location); // refresh
         });
     });
+    var tags = [
+        {label: '::1::', value: "::1::"}, 
+        {label: '::2::', value: "::2::"}
+    ];
     $('#message-area').bind('keyup', function(e) {
         var colon_key = 186; // the ":" key
         if(e.which != colon_key) {
             return true;
         }
         var latest_three = $(this).val().slice(-3);
+        var latest_two = latest_three.slice(-2);
         if(latest_three == ":::") {
             $('#task-create').dialog({modal: true});
+        } else if(latest_two == "::") {
+            
+        }
+    }).autocomplete({
+        minLength: 2,
+        delay: 0,
+        source: function(request, response) {
+            response($.ui.autocomplete.filter(tags, request.term.slice(-2))); // auto suggest based on last 2 chars
+        },
+        select: function(event, ui) {
+            var current_val = $('#message-area').val();
+            console.log(current_val);
+            var val = current_val.replace(/:*$/, '') + ui.item.value + ' ';
+            $('#message-area').val(val);
+            return false;
+        },
+        focus: function(event, ui) {
+            return false;       
         }
     });
     $('#create-task').click(function() {
