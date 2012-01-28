e95784f07bdda54dba329e2fb5fd7d72889bd97a
Gabi Purcaru <gabi@purcaru.com>
2012-01-28 12:24:26 +0200

    added login support

--- app.js
+++ app.js
@@ -11,20 +11,22 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
+app.configure(function() {
     app.set('views', settings.VIEWS);
     app.set('view engine', 'jade');
     app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: "#FFF#F#FSAA||}\"'" }));
     app.use(express.methodOverride());
     app.use(app.router);
-    app.use(express.static(settings.PUBLIC))
+    app.use(express.static(settings.PUBLIC));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
     app.use(express.errorHandler()); 
 });
 
@@ -32,6 +34,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.post('/ajax/', routes.ajax);
+app.get('/login/', routes.login);
 
 app.listen(settings.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,15 @@
 var Message = require('../messages').Message;
 var Task = require('../tasks').Task;
 var text_parse = require('../utils').text_parse;
+var settings = require('../settings');
 var async = require('async');
+var openid = require('openid');
 
 exports.index = function(req, res) {
+    if(!req.session || !req.session.user) {
+        res.redirect('/login/');
+        return;
+    }
     async.waterfall([
         function(callback) {
             Message.find({}, [], {sort: {date: -1}}, callback)
@@ -31,11 +37,50 @@ exports.index = function(req, res) {
     ]);
 };
 
+var extensions = [
+    new openid.UserInterface(),
+    new openid.AttributeExchange({
+        "http://axschema.org/contact/email": "required",
+        "http://axschema.org/namePerson/first": "required",
+        "http://axschema.org/namePerson/last": "required",
+    }),
+];
+var users = [
+    'gabi@purcaru.com',
+]
+var relyingParty = new openid.RelyingParty(settings.LOGIN_FULL_URL, null, false, false, extensions);
+exports.login = function(req, res) {
+    relyingParty.verifyAssertion(req, function(error, result) {
+        if(error || !result.authenticated) {
+            relyingParty.authenticate('https://www.google.com/accounts/o8/id', false, function(error, authUrl) {
+                if(error) {
+                    res.end('Error: ' + error);
+                } else if(!authUrl) {
+                    res.end('Authentication failed.');
+                } else {
+                    res.redirect(authUrl, 302);
+                }
+            });
+        } else {
+            if(users.indexOf(result.email) !== -1) {
+                req.session.user = {
+                    'email': result.email,
+                    'name': result.firstname + ' ' + result.lastname,
+                };
+                res.redirect('/');
+            } else {
+                res.end('You are not authorized.')
+            }
+        }
+    });
+}
+
 exports.ajax = function(req, res) {
     if(req.body.action == 'add_message') {
         message = new Message();
         message.content = req.body.content;
         message.date = new Date();
+        message.author = req.session.user.name;
         message.save(function(err) {
             if(err) {
                 res.send('Error occured.');
@@ -53,7 +98,7 @@ exports.ajax = function(req, res) {
             task.content = content;
             task.deadline = deadline;
             task.assigned_to = assigned_to;
-            task.author = "John Doe";
+            task.author = req.session.user.name;
             task.save();
             res.send(task.id);
         });
